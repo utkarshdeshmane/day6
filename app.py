4dde79b1ff5e91574baf20b804df548b67d32432
utkarshdeshmane <deshmaneuttu311@gmail.com>
2025-03-31 14:31:29 +0530

    done

--- app.py
+++ app.py
@@ -96,4 +96,4 @@ def home():
     return render_template("ajio_price_tracker.html", output=output, chart_json=chart_json)
 
 if __name__ == "__main__":
-    app.run(debug=True,port=8000,host="0.0.0.0")
+    app.run(debug=True,port=5000,host="0.0.0.0")
